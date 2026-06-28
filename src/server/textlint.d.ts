c0ddf8ee6eeefab115f42166857311cf140397cb
int314 <77012577+314systems@users.noreply.github.com>
2026-06-28 15:57:25 +0900

    Use @textlint/types

--- src/server/textlint.d.ts
+++ src/server/textlint.d.ts
@@ -1,48 +1,4 @@
-export interface TextLintFixCommand {
-  text: string;
-  range: [number, number];
-  isAbsolute: boolean;
-}
-
-export interface TextLintMessage {
-  // See src/shared/type/MessageType.js
-  // Message Type
-  type: string;
-  // Rule Id
-  ruleId: string;
-  message: string;
-  // optional data
-  data?: unknown;
-  // FixCommand
-  fix?: TextLintFixCommand;
-  // location info
-  // Text -> AST TxtNode(0-based columns) -> textlint -> TextLintMessage(**1-based columns**)
-  line: number; // start with 1
-  column: number; // start with 1
-  // indexed-location
-  index: number; // start with 0
-  // Severity Level
-  // See src/shared/type/SeverityLevel.js
-  severity?: number;
-}
-export interface TextlintFixResult {
-  filePath: string;
-  // fixed content
-  output: string;
-  // all messages = pre-applyingMessages + remainingMessages
-  // it is same with one of `TextlintResult`
-  messages: TextLintMessage[];
-  // applied fixable messages
-  applyingMessages: TextLintMessage[];
-  // original means original for applyingMessages and remainingMessages
-  // pre-applyingMessages + remainingMessages
-  remainingMessages: TextLintMessage[];
-}
-
-interface TextLintResult {
-  filePath: string;
-  messages: TextLintMessage[];
-}
+import type { TextlintFixResult, TextlintResult } from "@textlint/types";
 
 type ScanFilePathResult =
   | {
@@ -64,8 +20,8 @@ export type CreateLinterOptions = {
   cacheLocation?: string;
 };
 export type createLinter = (options: CreateLinterOptions) => {
-  lintFiles(files: string[]): Promise<TextLintResult[]>;
-  lintText(text: string, filePath: string): Promise<TextLintResult>;
+  lintFiles(files: string[]): Promise<TextlintResult[]>;
+  lintText(text: string, filePath: string): Promise<TextlintResult>;
   fixFiles(files: string[]): Promise<TextlintFixResult[]>;
   fixText(text: string, filePath: string): Promise<TextlintFixResult>;
   scanFilePath?(filePath: string): Promise<ScanFilePathResult>;
